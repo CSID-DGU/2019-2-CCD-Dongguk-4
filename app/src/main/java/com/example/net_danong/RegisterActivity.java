399f743b45b605861f8b6b1f46eaf4b448368946
gywlsl345 <gywlsl345@naver.com>
2019-11-08 22:54:23 +0900

    h

---
+++ app/src/main/java/com/example/net_danong/RegisterActivity.java
@@ -0,0 +1,25 @@
+package com.example.net_danong;
+
+import android.widget.Button;
+
+import androidx.appcompat.widget.AppCompatCheckBox;
+
+public class RegisterActivity {
+    //다음 진행 버튼
+    public Button nextBtn;
+
+    //체크박스 선택여부
+    public int TERM_AGREE_1 = 0;  //No check = 0, Check = 1
+    public int TERM_AGREE_2 = 0;
+    public int TERM_AGREE_3 = 0;
+    public int TERM_AGREE_4 = 0;
+
+    //체크박스
+    AppCompatCheckBox check1; //전체동의
+    AppCompatCheckBox check2; //첫벗째 동의
+    AppCompatCheckBox check3; //두번째 동의
+    AppCompatCheckBox check4; //세번째 동의
+
+    //전체동의
+
+}
